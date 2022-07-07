5af254a484f43e28e8cb15fa7bd7d85b68146edf
ArthurAlmeida1 <veigaarthurr@gmail.com>
2022-07-07 17:03:10 -0300

    Projeto finalizado

---
+++ src/boardgame/Board.java
@@ -0,0 +1,75 @@
+package boardgame;
+
+public class Board {
+
+	private int rows;
+	private int columns;
+	private Piece[][] pieces;
+	
+	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Ero ao criar tabuleiro: Deve colocar pelo menos 1 origem e 1 destino.");
+		}
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Piece[rows][columns];
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+	
+	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Posicao nao pretencve ao tabuleiro");
+		}
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao nao pretencve ao tabuleiro");
+		}
+		return pieces[position.getRow()][position.getColumn()];
+	}
+	
+	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("Ja existe uma peca na posicao " + position);
+		}
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
+	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao nao pretencve ao tabuleiro");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao nao pretencve ao tabuleiro");
+		}
+		return piece(position) != null;
+	}
+}
